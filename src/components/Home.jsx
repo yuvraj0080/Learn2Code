15444c899824abb639e8d18506f6d256c9806d84
hritikgoel26 <hritik.goel.ec.2018@miet.ac.in>
2022-09-27 15:48:42 +0530

    commit3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,8 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Link } from 'react-router-dom';
+import Footer from './Footer';
+
 
 
 const Home = () => {
@@ -33,6 +35,7 @@ const Home = () => {
               </div>
           </Carousel>
             </div>
+          <Footer></Footer>
           
           </div>
 
